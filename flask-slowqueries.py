9171dbc36a382c7c28d1741a16233251c9c91b46
Robert Elwell <robert@wikia-inc.com>
2013-05-17 09:36:02 -0700

    got flask app working and bootstrappified

--- flask-slowqueries.py
+++ flask-slowqueries.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask.ext.pymongo import PyMongo
 
-app = Flask(__name__)
+app = Flask('slowlr')
 mongo = PyMongo(app)
+
+app.debug = True
 
 @app.route('/')
 def home_page():
-    slow_queries = mongo.db.users.find({'online': True}).sort({'qtime':-1})
+    slow_queries = mongo.db.queries.find().sort('qtime', -1)
     return render_template('index.html', slow_queries=slow_queries)
 
 app.run()
